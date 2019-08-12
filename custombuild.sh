e4c77dac0ee544036fd56fdbb2cce85d25527aa4
Cristian Turcios <cristian.turcios@laureate.net>
2019-08-12 11:29:43 -0600

    finished migration process

--- custombuild.sh
+++ custombuild.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-ng build angular-app --prod --output-hashing=none && cat dist/angular-app/runtime.js dist/angular-app/polyfills.js dist/angular-app/scripts.js dist/angular-app/main.js > preview/angularapp.js
+ng build angular-app --prod --output-hashing=none && cat dist/angular-app/runtime-es5.js dist/angular-app/polyfills-es5.js dist/angular-app/scripts.js dist/angular-app/main-es5.js > preview/angularapp.js
